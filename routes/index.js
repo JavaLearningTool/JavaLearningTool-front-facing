05df0e302851fbea998fcf29fcba862edd47021e
Carl Schriever <crschriever@gmail.com>
2017-11-22 16:30:06 -0500

    Endpoint that accepts and compiles code

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+/* GET home page. */
+
+router.post('/compile', function (req, res, next) {
+  request.post({url: 'http://localhost:8080', method: "POST", form: {src: req.body.code}}, function (error, response, body) {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    console.log(body);
+    res.send(body);
+  });
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
