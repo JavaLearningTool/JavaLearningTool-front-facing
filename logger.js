cb1f10a40a16e443814579eed5803c32611d95c9
Carl Schriever <crschriever@gmail.com>
2018-01-23 19:36:08 -0500

    File based logging

--- logger.js
+++ logger.js
@@ -1,23 +1,46 @@
-var winston = require("winston");
+const winston = require("winston");
 winston.emitErrs = true;
 
-var logger = new winston.Logger({
-  transports: [
-    /*new winston.transports.File({
-      level: "info",
-      filename: "./logs/all-logs.log",
-      handleExceptions: true,
-      json: true,
-      maxsize: 5242880, //5MB
-      maxFiles: 5,
-      colorize: false
-    }),*/
-    new winston.transports.Console({
-      handleExceptions: true,
-      json: false,
-      colorize: true
-    })
-  ],
+let transports = [];
+
+if (process.env.DEV) {
+    transports.push(new winston.transports.Console({
+        handleExceptions: true,
+        json: false,
+        colorize: true
+      }));
+} else {
+    transports.push(new winston.transports.File({
+        name: "error-file",
+        level: "error",
+        filename: "./logs/error.log",
+        handleExceptions: true,
+        json: true,
+        maxsize: 5242880, //5MB
+        maxFiles: 3,
+        colorize: false,
+        timestamp: function() {
+          return new Date().toISOString();
+        }
+      }));
+
+    transports.push(new winston.transports.File({
+        name: "warning-file",
+        level: "warning",
+        filename: "./logs/warning.log",
+        handleExceptions: true,
+        json: true,
+        maxsize: 5242880, //5MB
+        maxFiles: 3,
+        colorize: false,
+        timestamp: function() {
+          return new Date().toISOString();
+        }
+      }));
+}
+
+const logger = new winston.Logger({
+  transports,
   exitOnError: false
 });
 
