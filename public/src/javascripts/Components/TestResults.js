dc7fd0c1a9d20aa1c795155d77531bb20db94d92
Carl Schriever <crschriever@gmail.com>
2018-02-07 10:33:44 -0500

    Display message type test results

--- public/src/javascripts/Components/TestResults.js
+++ public/src/javascripts/Components/TestResults.js
@@ -2,6 +2,9 @@
 
 import React from 'react';
 
+const MESSAGE_RESULT_TYPE = 'message';
+const COMPARATIVE_RESULT_TYPE = 'comparative';
+
 class TestResults extends React.Component {
     constructor(props) {
         super(props);
@@ -20,7 +23,7 @@ class TestResults extends React.Component {
         } else if (this.state.resultState.compiling) {
             return (<h1>Compiling ...</h1>);
         } else if (this.state.resultState.error) {
-            return (<pre><h1>{this.state.resultState.error}</h1></pre>);
+            return (<pre>{this.state.resultState.error}</pre>);
         } else {
             let items = [];
             this.state.resultState.data.forEach((element, index) => {
@@ -35,9 +38,10 @@ class TestResults extends React.Component {
     }
 
     renderTestResult(item, index) {
-        let passed = item.passed === 'true';
-        let timeout = item.timeout === 'true';
-        let runtimeException = item.runtimeException === 'true';
+        let testType = item.testType;
+        let passed = item.passed === "true";
+        let timeout = item.timeout === "true";
+        let runtimeException = item.runtimeException === "true";
 
         let passFailLabel = "You passed!";
         if (timeout) {
@@ -48,78 +52,87 @@ class TestResults extends React.Component {
             passFailLabel = "You failed.";            
         }
 
-        let inputLabel;
-        if (item.input === "") {
-            inputLabel = <p className="resultLabel"> Input: None </p>
-        } else {
-            inputLabel = <p className="resultLabel"> Input: {item.input} </p>;            
-        }
-
+        let resultLabel;
         let body;
-        if (!timeout && !runtimeException) { // Passed or failed normally
-            body = <div className="drop-down-body ">
-                <div className="resultArea">
-                    <p className="resultLabel">Expected: </p>
-                    <p className="resultField">
-                        {this.replaceQuotes(this.replaceNewLines(item.expected))}
-                    </p>
-                </div>
-                <div className="resultArea">
-                    <p className="resultLabel">Actual: </p>
-                    <p className="resultField">
-                        {this.replaceQuotes(this.replaceNewLines(item.actual))}
-                    </p>
-                </div>
-                <p className="resultLabel"> time: {item.time} ms</p>
-              </div>;
-        } else if (timeout) { // Timeout occurred
-            body = <div className="drop-down-body ">
-                <div className="resultArea">
-                    <p className="resultLabel"> Timeout! after {item.time} ms.</p>
-                </div>
-                <br/>
-                <div className="resultArea">
-                    <p className="resultLabel">Expected: </p>
-                    <p className="resultField">
-                        {this.replaceQuotes(this.replaceNewLines(item.expected))}
-                    </p>
-                </div>
-              </div>;
-        } else { // Runtime exception
-            body = <div className="drop-down-body ">
-                <div className="resultArea">
-                    <p className="resultLabel">Compiler Message: </p>
-                    <pre className="resultField">
-                        {this.replaceQuotes(item.exceptionMessage)}
-                    </pre>
-                </div>
-                <div className="resultArea">
-                    <p className="resultLabel">Expected: </p>
-                    <p className="resultField">
-                        {this.replaceQuotes(this.replaceNewLines(item.expected))}
-                    </p>
-                </div>
-                <p className="resultLabel"> time: {item.time} ms</p>
-              </div>;
-        }
 
-        return <div className={"testResult drop-down-container " + (item.dropped ? "dropped" : "")} key={index} onClick={e => {
-              this.dropDownClicked(index);
-            }}>
-            <div className="drop-down-header">
-              <div className="headerResultArea">
-                <p
-                  className={
-                    "passedLabel " + (passed ? "success" : "failure")
-                  }
-                >
-                  {passFailLabel}
-                </p>
-                {inputLabel}
-              </div>
-            </div>
-            {body}
-          </div>;
+        if (testType === MESSAGE_RESULT_TYPE) {
+            resultLabel = <p className="resultLabel"> {item.label} </p>
+
+            body = <div className="drop-down-body ">
+                    <div className="resultArea">
+                        <p className="resultLabel">Message: </p>
+                        <p className="resultField">
+                            {this.replaceQuotes(this.replaceNewLines(item.message))}
+                        </p>
+                    </div>
+                    <p className="resultLabel"> time: {item.time} ms</p>
+                </div>;
+        } else {
+            if (item.input === "") {
+                resultLabel = <p className="resultLabel"> Input: None </p>
+            } else {
+                resultLabel = <p className="resultLabel"> Input: {item.input} </p>;            
+            }
+
+            if (!timeout && !runtimeException) { // Passed or failed normally
+                body = <div className="drop-down-body ">
+                    <div className="resultArea">
+                        <p className="resultLabel">Expected: </p>
+                        <p className="resultField">
+                            {this.replaceQuotes(this.replaceNewLines(item.expected))}
+                        </p>
+                    </div>
+                    <div className="resultArea">
+                        <p className="resultLabel">Actual: </p>
+                        <p className="resultField">
+                            {this.replaceQuotes(this.replaceNewLines(item.actual))}
+                        </p>
+                    </div>
+                    <p className="resultLabel"> time: {item.time} ms</p>
+                </div>;
+            } else if (timeout) { // Timeout occurred
+                body = <div className="drop-down-body ">
+                    <div className="resultArea">
+                        <p className="resultLabel"> Timeout! after {item.time} ms.</p>
+                    </div>
+                    <br/>
+                    <div className="resultArea">
+                        <p className="resultLabel">Expected: </p>
+                        <p className="resultField">
+                            {this.replaceQuotes(this.replaceNewLines(item.expected))}
+                        </p>
+                    </div>
+                </div>;
+            } else { // Runtime exception
+                body = <div className="drop-down-body ">
+                    <div className="resultArea">
+                        <p className="resultLabel">Compiler Message: </p>
+                        <pre className="resultField">
+                            {this.replaceQuotes(item.exceptionMessage)}
+                        </pre>
+                    </div>
+                    <div className="resultArea">
+                        <p className="resultLabel">Expected: </p>
+                        <p className="resultField">
+                            {this.replaceQuotes(this.replaceNewLines(item.expected))}
+                        </p>
+                    </div>
+                    <p className="resultLabel"> time: {item.time} ms</p>
+                </div>;
+            }
+        }
+        
+        return <div className={"testResult drop-down-container " + (item.dropped ? "dropped" : "")} key={index} onClick={e => {this.dropDownClicked(index);}}>
+                <div className="drop-down-header">
+                    <div className="headerResultArea">
+                        <p className={"passedLabel " + (passed ? "success" : "failure")}>
+                            {passFailLabel}
+                        </p>
+                        {resultLabel}
+                    </div>
+                </div>
+                {body}
+            </div>;
     }
 
     dropDownClicked(index) {
