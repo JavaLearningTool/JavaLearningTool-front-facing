3a95ec4106fcdf510e8b1acae815d43b2d606ef5
crschriever <crschriever@gmail.com>
2018-03-26 22:55:00 -0400

    Use localStorage API instead of Cookies for saving Challenges

--- public/src/javascripts/challenge.js
+++ public/src/javascripts/challenge.js
@@ -1,48 +1,63 @@
-'use strict';
+"use strict";
 
-import * as Cookies from 'js-cookie';
-import { setInterval } from 'timers';
+import { setInterval } from "timers";
 
 let shouldSave = false;
 let codeMirror;
 let changeSpan;
 
+function store(path, value) {
+    if (window.localStorage) {
+        window.localStorage.setItem(path, value);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function load(path) {
+    if (window.localStorage) {
+        return window.localStorage.getItem(path);
+    }
+}
+
 window.onload = function() {
-    changeSpan = document.getElementsByClassName('saving')[0];
+    changeSpan = document.getElementsByClassName("saving")[0];
 
     setInterval(() => {
         if (shouldSave) {
-            Cookies.set(window.challengePath + "_save", codeMirror.getValue());
-            shouldSave = false;
-            changeSpan.innerHTML = 'Changes saved';
+            if (!store(window.challengePath + "_save", codeMirror.getValue())) {
+                changeSpan.innerHTML = "Saving not supported in this browser.";
+            } else {
+                shouldSave = false;
+                changeSpan.innerHTML = "Changes saved";
+            }
         }
     }, 1500);
 
     if (window.readOnlyLines) {
-
         // listen for the beforeChange event, test the changed line number, and cancel
-        document.codeMirror.on('beforeChange',function(cm, change) {
+        document.codeMirror.on("beforeChange", function(cm, change) {
             if (window.readOnlyLines.indexOf(change.from.line) >= 0) {
                 change.cancel();
             }
         });
-
     }
 
-    document.codeMirror.on('change', (cm) => {
+    document.codeMirror.on("change", cm => {
         codeMirror = cm;
         shouldSave = true;
-    
-        changeSpan.innerHTML = 'Saving ...';
-    });
-}
 
-window.onCodeMirrorLoad = (cm) => {
-    let savedText = Cookies.get(window.challengePath + '_save', {expires: 100});
+        changeSpan.innerHTML = "Saving ...";
+    });
+};
+
+window.onCodeMirrorLoad = cm => {
+    let savedText = load(window.challengePath + "_save");
     if (savedText) {
         cm.getDoc().setValue(savedText);
     }
-}
+};
 
 let compileButton;
 
@@ -55,6 +70,8 @@ window.codeCompilationStarted = () => {
 };
 
 window.window.codeCompilationEnded = () => {
-    compileButton.className = compileButton.className
-        .replace(/(?:^|\s)pure-button-disabled(?!\S)/g, "");
+    compileButton.className = compileButton.className.replace(
+        /(?:^|\s)pure-button-disabled(?!\S)/g,
+        ""
+    );
 };
